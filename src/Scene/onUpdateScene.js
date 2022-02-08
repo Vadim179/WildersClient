5e2e9265a7398c4589e76f7d01a9722ca9268b9b
Vadim Vengriniuc <54371115+Vadim179@users.noreply.github.com>
2022-02-08 21:01:36 +0200

    Remove Movement Controller

--- src/Scene/onUpdateScene.js
+++ src/Scene/onUpdateScene.js
@@ -1,11 +1,7 @@
-import {
-  RotationControls,
-  MovementControls,
-} from 'Controls'
+import { RotationControls } from 'Controls'
 
 function onUpdateScene() {
   RotationControls.doRotatePlayer.call(this)
-  MovementControls.doMovePlayer.call(this)
 }
 
 export default onUpdateScene
